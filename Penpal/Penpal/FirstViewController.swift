44df77fdeb36e217110a2688f5bfd3e2f4185de4
Wentao Xu <wentaoxu@apples-mbp.hbs.edu>
2015-01-20 14:50:36 -0500

    Initial Commit

---
+++ Penpal/Penpal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Penpal
+//
+//  Created by Wentao Xu on 1/20/15.
+//  Copyright (c) 2015 W16 Design Studio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Penpal/PenpalTests/PenpalTests.swift
@@ -0,0 +1,36 @@
+//
+//  PenpalTests.swift
+//  PenpalTests
+//
+//  Created by Wentao Xu on 1/20/15.
+//  Copyright (c) 2015 W16 Design Studio. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PenpalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
