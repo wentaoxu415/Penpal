689a5fefebf97e24f9b264f763714cf2192d883e
Wentao Xu <wentaoxu@apples-MacBook-Pro.local>
2015-01-20 21:24:26 -0500

    Tuesday work

--- Penpal/Penpal/FirstViewController.swift
+++ Penpal/Penpal/FirstViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
+
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector,(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func keyboardWasShown(notification: NSNotification) {
+        var info = notification.userInfo!
+        var keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.bottomConstraint.constant = 280 + 20
+        })
     }
 
 
